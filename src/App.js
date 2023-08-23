5ce0c2aa6ea63dde30adc95b8d3231e986aa4e51
linhtiendev <lelinhtien0707@gmail.com>
2023-08-23 13:36:49 +0700

    handle onClick event

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import "./App.scss";
 import Nav from "./views/Nav";
 
-function App() {
+const App = () => {
+    const handleEventClick = (event) => {
+        console.log("click me", event.target.value);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
                 <Nav />
                 <h1>Hello World</h1>
+                <input
+                    type="text"
+                    value="Liti"
+                    onClick={(event) => handleEventClick(event)}
+                />
+                <button
+                    type="button"
+                    onClick={(event) => handleEventClick(event)}
+                >
+                    Click me
+                </button>
             </header>
         </div>
     );
-}
+};
 
 export default App;
