cfd6012fc5f706248f37d09a2ba64cc2ca475b80
linhtiendev <lelinhtien0707@gmail.com>
2023-08-23 12:12:39 +0700

    multiple components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import "./App.css";
+import "./App.scss";
+import Nav from "./views/Nav";
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
+                <Nav />
                 <h1>Hello World</h1>
             </header>
         </div>
