ac2671849fed5dfa55eaadb94faa08e4a9db334f
linhtiendev <lelinhtien0707@gmail.com>
2023-08-24 16:43:30 +0700

    conditional loading message

--- src/views/InfoUser.js
+++ src/views/InfoUser.js
@@ -7,18 +7,25 @@ import "./InfoUser.scss";
 const InfoCovid = () => {
     // tạo state có array rỗng
     const [dataUser, setDataUser] = useState([]);
+    // Biến kiểm tra trạng thái loading data
+    const [loading, setLoading] = useState(true);
     // dùng useEffect để lấy data <=> componentDidMount
     useEffect(async () => {
-        let res = await axios.get("https://reqres.in/api/users?page=1");
-        // kiểm tra giá trị data, nếu không có sẽ in ra mảng rỗng
-        let data = res && res.data && res.data.data ? res.data.data : [];
-        // nếu có data sẽ set lại state
-        setDataUser(data);
+        setTimeout(async () => {
+            let res = await axios.get("https://reqres.in/api/users?page=1");
+            // kiểm tra giá trị data, nếu không có sẽ in ra mảng rỗng
+            let data = res && res.data && res.data.data ? res.data.data : [];
+            // nếu có data sẽ set lại state
+            setDataUser(data);
+            // hàm sẽ chạy khi loading = false
+            setLoading(false);
+        }, 3000);
         // truyền vào 1 mảng rỗng để hàm chỉ chạy một lần
     }, []);
 
     return (
         <div className="info-container">
+            <h2>Table Users</h2>
             <table>
                 {console.log("check data", dataUser)}
                 <thead>
@@ -31,7 +38,9 @@ const InfoCovid = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {dataUser &&
+                    {/* Nếu loading === false */}
+                    {loading === false &&
+                        dataUser &&
                         dataUser.length > 0 &&
                         dataUser.map((item, index) => {
                             return (
@@ -49,6 +58,14 @@ const InfoCovid = () => {
                                 </tr>
                             );
                         })}
+                    {/* nếu loading === true */}
+                    {loading === true && (
+                        <tr>
+                            <td colSpan="5" style={{ textAlign: "center" }}>
+                                Loading data...
+                            </td>
+                        </tr>
+                    )}
                 </tbody>
             </table>
         </div>
