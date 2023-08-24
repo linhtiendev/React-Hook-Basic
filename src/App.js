89723767aa42bcaff1887ab6448a60e97f7508b6
linhtiendev <lelinhtien0707@gmail.com>
2023-08-24 12:22:28 +0700

    function as props (child using function parent)

--- src/App.js
+++ src/App.js
@@ -22,7 +22,11 @@ const App = () => {
             return;
         }
         // hook not merge state vì vậy phải dùng ... (spread syntax)
-        let newTodo = { id: "123", title: address, type: "linhtiendev" }; // tạo ra 1 mảng mới
+        let newTodo = {
+            id: Math.floor(Math.random() * 10000 + 1),
+            title: address,
+            type: "linhtiendev",
+        }; // tạo ra 1 mảng mới
         setTodos([...todos, newTodo]); // dùng toán tử ... để lấy obj cũ, lấy thêm obj vừa nhập
         setAddress(""); // set input thành rỗng
     };
@@ -34,19 +38,33 @@ const App = () => {
         setAddress(event.target.value);
     };
 
+    // hàm xóa
+    const deleteDataTodo = (id) => {
+        // tạo biến mới để hứng todos
+        let currentTodo = todos;
+        currentTodo = currentTodo.filter((item) => item.id !== id);
+        setTodos(currentTodo);
+    };
+
     return (
         <div className="App">
             <header className="App-header">
                 <Nav />
                 <h1>Hello World {name}</h1>
                 {/* truyền props */}
-                <Todo myData={todos} title={"all todo"} />
+                <Todo
+                    todos={todos}
+                    title={"all todo"}
+                    // truyền props delete
+                    deleteDataTodo={deleteDataTodo}
+                />
                 {/* tai sd Todo */}
                 {/* Dùng hàm filter để lọc mảng có type là linhtiendev */}
                 {/* có input đầu vào là type */}
                 <Todo
-                    myData={todos.filter((item) => item.type === "linhtiendev")}
+                    todos={todos.filter((item) => item.type === "linhtiendev")}
                     title={`linhtiendev's todos`}
+                    deleteDataTodo={deleteDataTodo}
                 />
                 <input
                     type="text"
--- src/views/Todo.js
+++ src/views/Todo.js
@@ -1,21 +1,38 @@
-const Todo = (props) => {
+// props được truyền xuống
+const Todo = ({ todos, title, deleteDataTodo }) => {
     // propreties
     // truyền dữ liệu từ parent => child <=> top => bottom
 
     // khi parent thay đổi thì child thay đổi theo
     // nhận props được truyền xuống
-    const todos = props.myData;
+
+    // cách 1: const todos = props.todos;
+    // cách 2: const { todos, title } = props;
+
+    // Hàm delete
+    const handleOnclickDelete = (id) => {
+        // nhận props được truyền từ cha
+        deleteDataTodo(id);
+    };
+
     return (
         <div className="todo-container">
             {/* Nhận props title */}
-            <div className="title">{props.title}</div>
+            <div className="title">{title}</div>
             {/* kh dùng for với for-each vì nó sẽ thay đổi phần tử ở mảng cũ */}
             {/* Dùng vòng map để lập 1 mảng mới */}
             {todos.map((todo) => {
                 return (
-                    <li className="todo-content" key={todo.id}>
-                        {todo.title}
-                    </li>
+                    <div key={todo.id}>
+                        <li className="todo-content">
+                            {todo.title}{" "}
+                            <button
+                                onClick={() => handleOnclickDelete(todo.id)}
+                            >
+                                x
+                            </button>
+                        </li>
+                    </div>
                 );
             })}
             <hr />
