244ec60484b88efa19d3a66ef282f4bc2aeaad51
linhtiendev <lelinhtien0707@gmail.com>
2023-08-25 18:32:27 +0700

    using useHistory to redirect

--- src/views/DetailBlog.js
+++ src/views/DetailBlog.js
@@ -1,10 +1,23 @@
-import { useParams } from "react-router-dom";
+// useParams để lấy được tham số id
+// useHistory để lưu lại dữ liệu -> khi chuyển trang r back lại sẽ kh phải load
+import { useParams, useHistory } from "react-router-dom";
 import "./DetailBlog.scss";
 
 const DetailBlog = () => {
     let { id } = useParams();
+    let history = useHistory();
+
+    const handleBackData = () => {
+        // click để trở lại route blog
+        history.push("/blog");
+    };
     return (
         <>
+            <div>
+                <button type="button" onClick={() => handleBackData()}>
+                    Back
+                </button>
+            </div>
             <div>detail id: {id}</div>
         </>
     );
